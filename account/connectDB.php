9b5c332cb8d6fe983091300f94a17af9ff001329
seasunrise22 <seasunrise22@gmail.com>
2019-04-20 01:03:44 +0900

    connectDB 필요없는 부분 삭제

--- account/connectDB.php
+++ account/connectDB.php
@@ -1,15 +1,9 @@
 <?php
 // DB연결용 세팅
 $host 		= 'localhost';
-<<<<<<< HEAD
 $username 	= 'seasunrise22';
 $password 	= 'mydbpw123';
 $dbname 	= 'seasunrise22';
-=======
-$username 	= 'for github'; // 
-$password 	= 'for github'; // 
-$dbname 	= 'for github'; // 
->>>>>>> e689a7334815d6809036c8a1f420703251546c0f
 
 $mysqli = new mysqli($host, $username, $password, $dbname);
 
